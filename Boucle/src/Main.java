e960f356ac2c2e3effd49d0756a1093c0dc6465e
JonathanVieusses <vieusses.jonathan@gmail.com>
2025-10-27 17:02:12 +0100

    fin de journée 27/10/2025. etape 1 fini

---
+++ Boucle/src/Main.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+// -------------------- ETAPE 2 --------------------
+
+        // exercice 1
+
+        // solution 1 avec "do" et "while"
+
+        /*
+
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while (i <= 10);
+
+        */
+
+
+        // solution 2 avec "for"
+
+        /*
+
+        for ( int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        */
+
+
+
+        // exercice 2
+
+        // solution 1 avec "for"
+
+        /*
+
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+
+        */
+
+        // solution 2 avec "countdown"
+
+        /*
+
+        int countdown = 10;
+
+        while (countdown >= 0) {
+            System.out.println(countdown);
+            countdown--;
+        }
+
+         */
+
+    }
+
+}
--- Else If/src/Main.java
+++ Else If/src/Main.java
@@ -1,5 +1,89 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("test");
+
+
+
+        // ------------------------------ ETAPE 1 -------------------------------
+
+        // exercice 1
+
+        /*
+
+        Scanner etape1a = new Scanner(System.in);
+
+        System.out.println("Choisissez un nombre : ");
+        double x = etape1a.nextInt();
+
+        if (x > 0) {
+            System.out.println("Le nombre est positif");
+        }
+
+        else if (x < 0) {
+            System.out.println("Le nombre est negatif");
+        }
+
+        else if (x == 0) {
+            System.out.println("le nombre est nul");
+        }
+        etape1a.close();
+
+        */
+
+        // exercice 3
+
+        /*
+
+        Scanner exo3 =new Scanner(System.in);
+        System.out.println("choissisez une note");
+        double x = exo3.nextDouble();
+
+        if (x >= 16) {
+            System.out.println("Trés bien");
+        }
+
+        else if (x >= 12) {
+            System.out.println("Bien");
+        }
+
+        else if (x >= 10) {
+            System.out.println("Assez bien");
+        }
+
+        else {
+            System.out.println("Insuffisant");
+        }
+        exo3.close();
+
+        */
+
+        // exercice 4
+
+        /*
+
+        Scanner exo4 =  new Scanner(System.in);
+        System.out.println("Choisissez le premier nombre : ");
+        double x = exo4.nextDouble();
+
+        System.out.println("Choisissez votre deuxiéme nombre");
+        double y = exo4.nextDouble();
+
+        if (x > y) {
+            System.out.println("Le plus grand est : " + x);
+        }
+
+        else if (y > x) {
+            System.out.println("Le plus grand est  : " +y);
+        }
+
+        else if (x == y) {
+        System.out.println("Ils sont égaux");
+        }
+
+        */
+
     }
 }
